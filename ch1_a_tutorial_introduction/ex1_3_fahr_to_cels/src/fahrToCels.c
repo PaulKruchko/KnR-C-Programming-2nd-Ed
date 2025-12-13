cbaa7d7c28673cbf4632a153f9d8366a1c85be36
Paul Kruchko <pmkruchko@gmail.com>
2025-12-13 12:07:49 -0500

    main: Added excercise 1-3

---
+++ ch1_a_tutorial_introduction/ex1_3_fahr_to_cels/src/fahrToCels.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/**
+ * @brief print Fehrenheit-Celsius table for fahr = 0, 20, ..., 300 
+ *  
+ * This temperature conversion uses a linear slope-intercept form,
+ * y = mx + b, where: 
+ * y = Tempearture in degrees celsius
+ * m = slope or scaling factor
+ * x = Temperatuer in degrees celsius
+ * b = offset between the two measurement scales
+ * 
+ * @param [out] result Indicates if the program executed successfully or not
+ * @param [in] void 
+*/
+
+#define SUCCESS 0U
+#define FAILURE 1U
+#define ASCII_SPACE 32U
+
+int main(void)
+{
+    float fahr, celsius, offset, scale;
+    int lower, upper, step, result;
+
+    lower  = 0;       /* lower limit of temperature table */
+    upper  = 300;     /* upper limit*/
+    step   = 20;      /* step size */
+    offset = 32.0;      /* offset for conversion formula */
+    scale  = 5.0/9.0; /* scale for the conversion formula */ 
+    result = SUCCESS;
+
+    fahr = lower; /* init fahrenheit scale with the lowerst range */
+    
+    printf("/********************************************/\n");
+	printf("*********** Fahrenheit to Celcius ************\n");
+	printf("/********************************************/\n");
+	
+	printf("Degrees[F]    Degrees[C]\n");
+
+    while (fahr <= upper)
+    {
+        celsius = scale * (fahr - offset);   /* convert degrees fahrenheit into degrees celsius */
+        printf("%c%c%3.0f\t\t%6.1f\n",ASCII_SPACE, ASCII_SPACE,fahr, celsius); 
+        fahr = fahr + step;                  /* increment degrees fahrenheit by step size */
+    }
+
+    return result;
+}
